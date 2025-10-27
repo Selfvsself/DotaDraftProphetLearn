105cc66300561f3ba514f137dff048efc648ac10
Selfvsself <killerman323@gmail.com>
2025-10-27 10:25:43 +0300

    upd train_model.py, add models folder

---
+++ models/mlp_dota_model_medium.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+
+class DotaModel(nn.Module):
+    def __init__(self, num_heroes, emb_dim=128, dropout_p=0.5):
+        super(DotaModel, self).__init__()
+        self.hero_emb = nn.Embedding(
+            num_embeddings=num_heroes,
+            embedding_dim=emb_dim
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(2 * emb_dim + 3, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(1024, 32),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(32, 1)
+        )
+        self.dropout = nn.Dropout(p=dropout_p)
+
+    def forward(self, radiant_ids, dire_ids, avg_rank_tiers, num_rank_tiers, durations):
+        emb_r = self.hero_emb(radiant_ids)
+        emb_d = self.hero_emb(dire_ids)
+        team_r = emb_r.mean(dim=1)
+        team_d = emb_d.mean(dim=1)
+        x = torch.cat([team_r,
+                       team_d,
+                       avg_rank_tiers.unsqueeze(1),
+                       num_rank_tiers.unsqueeze(1),
+                       durations.unsqueeze(1)],
+                      dim=1)
+        x = self.dropout(x)
+        logit = self.fc(x)
+        return logit.squeeze(1)
--- train_model.py
+++ train_model.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
+from models.mlp_dota_model_medium import DotaModel
 
 
 class DotaDataset(Dataset):
@@ -37,40 +38,6 @@ class DotaDataset(Dataset):
             self.y[idx]
 
 
-class DotaModel(nn.Module):
-    def __init__(self, num_heroes, emb_dim=32, dropout_p=0.3):
-        super(DotaModel, self).__init__()
-        self.hero_emb = nn.Embedding(
-            num_embeddings=num_heroes,
-            embedding_dim=emb_dim
-        )
-        self.fc = nn.Sequential(
-            nn.Linear(2 * emb_dim + 3, 256),
-            nn.ReLU(),
-            nn.Dropout(p=dropout_p),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Dropout(p=dropout_p),
-            nn.Linear(128, 1)
-        )
-        self.dropout = nn.Dropout(p=dropout_p)
-
-    def forward(self, radiant_ids, dire_ids, avg_rank_tiers, num_rank_tiers, durations):
-        emb_r = self.hero_emb(radiant_ids)
-        emb_d = self.hero_emb(dire_ids)
-        team_r = emb_r.mean(dim=1)
-        team_d = emb_d.mean(dim=1)
-        x = torch.cat([team_r,
-                       team_d,
-                       avg_rank_tiers.unsqueeze(1),
-                       num_rank_tiers.unsqueeze(1),
-                       durations.unsqueeze(1)],
-                      dim=1)
-        x = self.dropout(x)
-        logit = self.fc(x)
-        return logit.squeeze(1)
-
-
 output_dir = f'train/{time.strftime("%Y%m%d-%H%M%S")}'
 os.makedirs(output_dir, exist_ok=True)
 
@@ -90,7 +57,11 @@ BATCH_SIZE = 128
 NUM_HEROES = 126
 LEARNING_RATE = 0.001
 L2_REGULARIZATION = 0.0001
-NUM_EPOCHS = 10
+NUM_EPOCHS = 50
+
+logging.info(f"Batch size: {BATCH_SIZE}")
+logging.info(f"Learning rate: {LEARNING_RATE}")
+logging.info(f"L2 regularization: {L2_REGULARIZATION}")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 logging.info(f"Using device: {device}")
@@ -179,7 +150,7 @@ for epoch in range(NUM_EPOCHS):
 
     if val_acc > VAL_ACC_MAX:
         BEST_MODEL_FILENAME = f'model_{epoch + 1}_{avg_loss:.4f}_{train_acc:.3f}_' \
-                              f'{val_loss / len(val_loader):.4f}_{val_acc:.3f}.pth'
+                              f'{val_loss:.4f}_{val_acc:.3f}.pth'
         torch.save(model.state_dict(), os.path.join(output_dir, BEST_MODEL_FILENAME))
         VAL_ACC_MAX = val_acc
         logging.info(
